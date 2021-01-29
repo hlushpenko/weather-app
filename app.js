c74981b15e5f06e194055d8cdf2c9ed58c252c83
nick <nick.hlushpenko@gmail.com>
2021-01-29 20:51:53 +0200

    Fix API

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ function showError(error){
 
 // GET WEATHER FROM API PROVIDER
 function getWeather(latitude, longitude){
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&lang=ua`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&lang=ua`;
     
     fetch(api)
         .then(function(response){
